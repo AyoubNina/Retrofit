f5360fad3c9e3556307347772b7c67f360106b9c
AyoubNina <jihene.ayoub@gmail.com>
2023-02-14 15:03:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/retrofit/Produit.java
@@ -0,0 +1,13 @@
+package com.example.retrofit;
+
+public class Produit {
+    private String nomProduit;
+
+    public Produit(String nomProduit) {
+        this.nomProduit = nomProduit;
+    }
+    public String getNomProduit() {
+        return nomProduit;
+    }
+
+}
